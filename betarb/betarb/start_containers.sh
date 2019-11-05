53b615f5db00e3626c175dfa1106d748d0762fc5
Jaco Jansen <jaco.jansen@iress.com.au>
2019-11-05 22:02:58 +0000

    cleanup

--- betarb/betarb/start_containers.sh
+++
@@ -1,7 +0,0 @@
-rm -rf /var/tmp/django_cache
-docker run --rm -d --name reddy -p 6379:6379 redis
-docker run --rm -d --name haas  -p 5672:5672 -p 15672:15672 --hostname haashost --link reddy:redis rabbitmq:management
-sleep 4
-docker ps
-sleep 4
-celery -A betarb worker -l info -B --scheduler django_celery_beat.schedulers:DatabaseScheduler --concurrency=1
